b4c9f548ec0769036f8c97b235eb21d58005db6f
Pravin Lokhande <pravin.lokhande@cuelogic.com>
2019-03-15 19:29:42 +0530

    style added to Quotes using custome method.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  // app.import('node_modules/font-awesome/css/font-awesome.css', {destDir: 'assets'});
+  // app.import('node_modules/bootstrap/dist/css/bootstrap.css', {destDir: 'assets'});
   return app.toTree();
 };
